ecf1b23ef1140b7ead27ea99013b03caa4775e83
Sven Kleinert <sven.kleinert@phoenixd.uni-hannover.de>
2024-06-05 14:15:08 +0200

    IHS 13.0.1. Fixed installation bug. Backup scripts improved.

--- cronjobs/backup_db.sh
+++ cronjobs/backup_db.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+VERBOSE=false
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 . $SCRIPT_DIR/../.env
 
+if [ "${1}" == "-h" ] || [ $# -lt 1 ]; then
+	echo "Usage: backupe_zope.sh <backup-directory>"
+	echo "	backup-directory - Verzeichnis in welches das Backup gespeichert wird."
+	echo ""
+	exit 0
+fi
+
 BACKUPDIR="${1}"
+VERBOSE="${2}"
 
 DATE=`date +%F_%T`
 HOSTNAME=`hostname -f`
 
 FILE="${BACKUPDIR}/${HOSTNAME}_IHS-${NAME}_${DATE}.zexp"
+if [ $VERBOSE ]; then
+	echo -n "Dumping database to ${BACKUPDIR}/${DATE}_${HOSTNAME}-db.sql.gz..."
+fi
 mysqldump --all-databases --host=127.0.0.1 --password=${MARIADB_ROOT_PASSWORD} | gzip -c > ${BACKUPDIR}/${DATE}_${HOSTNAME}-db.sql.gz
+if [ $VERBOSE ]; then
+	echo " done!"
+fi
--- cronjobs/backup_zope.sh
+++ cronjobs/backup_zope.sh
@@ -4,14 +4,46 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 . $SCRIPT_DIR/../.env
 
+if [ "${1}" == "-h" ] || [ $# -lt 2 ]; then
+	echo "Usage: backupe_zope.sh <acronym> <backup-directory>"
+	echo "	acronym - Instutskuerzel, wie es auch im IHS verwendet wird (Gross/Kleinschreibung beachten!)"
+	echo "	backup-directory - Verzeichnis in welches das Backup gespeichert wird."
+	echo ""
+	exit 0
+fi
+
 NAME="${1}"
 BACKUPDIR="${2}"
+VERBOSE="${3}"
 
 DATE=`date +%F_%T`
 HOSTNAME=`hostname -f`
 
 AUTH="Authorization: basic `echo -n "root:${ZOPE_ROOT_PASSWORD}" | base64`"
 URL="http://localhost:8080/Select/install/export_py?kurztext=${NAME}"
-FILE="${BACKUPDIR}/${HOSTNAME}_IHS-${NAME}_${DATE}.zexp.gz"
-echo "Storing ZODB backup of /IHS-${NAME} in ${FILE}..."
-wget --quiet -O - --header="$AUTH" $URL | gzip -c > "${FILE}"
+FILE="${BACKUPDIR}/${HOSTNAME}_IHS-${NAME}_${DATE}.zexp"
+if [ $VERBOSE ]; then
+	echo -n "Storing ZODB backup of /IHS-${NAME} in ${FILE}.gz..."
+fi
+
+HTTP_CODE=$(wget --spider -S --header="$AUTH" $URL 2>&1 | grep "HTTP/" | awk '{print $2}')
+
+if [ $HTTP_CODE != "200" ]; then
+	echo "ERROR: Webserver returned HTTP code ${HTTP_CODE}. Check if the server is running properly."
+	echo "NO BACKUP WAS SAVED!!!"
+	exit 1
+fi
+
+wget --quiet -O - --header="$AUTH" $URL > "${FILE}"
+
+if [ ! -s "${FILE}" ]; then
+	echo "ERROR: The file seems to be empty! Check if the acronym is correct!"
+	echo "NO BACKUP WAS SAVED!!!"
+	exit 1
+fi
+
+gzip "${FILE}"
+
+if [ $VERBOSE ]; then
+	echo " done!"
+fi
